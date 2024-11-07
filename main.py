f944cbd730ee28b13745c3a41a0fd83247d87b83
nils_borsboom <c.d.borsboom@ziggo.nl>
2024-11-07 14:13:09 +0100

    count character function added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     text_name = "books/frankenstein.txt"
     text_file = read_file(text_name)
     print(count_words(text_file))
+    print(count_characters(text_file))
     
 def count_words(text_file):
     words = text_file.split()
@@ -11,5 +12,15 @@ def read_file(text_name):
     with open(text_name) as f:
         file_contents = f.read()
         return file_contents
+    
+def count_characters(text_file):
+    text_file_lowerstrings = text_file.lower()
+    character_dict = {}
+    for character in text_file_lowerstrings:
+        if character not in character_dict:
+            character_dict.update({character: 1})
+        else:
+            character_dict[character] += 1
+    return character_dict
 
 main()
