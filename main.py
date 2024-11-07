d07be99accd23cc8f51a3f148931d46b489fb5ae
nils_borsboom <c.d.borsboom@ziggo.nl>
2024-11-07 12:07:46 +0100

    split read and count into two functions

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
     text_name = "books/frankenstein.txt"
-    count_words(text_name)
+    text_file = read_file(text_name)
+    print(count_words(text_file))
     
-def count_words(text_name):
+def count_words(text_file):
+    words = text_file.split()
+    return len(words)
+        
+def read_file(text_name):
     with open(text_name) as f:
         file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
-        
+        return file_contents
+
 main()
